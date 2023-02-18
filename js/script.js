3b382bd12165edfd8cf94460d6a1bc83ea9629d7
farrux13x <abdusalomovf1208@gmail.com>
2023-02-18 15:16:38 +0500

    image style fixed

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector('.service__sircle')
+let style = document.querySelector('.service__b__text')
+function none(){
+    console.log('s')
+    style.classList.add('none')
+}
